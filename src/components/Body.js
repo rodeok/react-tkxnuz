daee3efd2fa8a305a90a4ef47c05cd38f68261ae
rodeok <nationx22@gmail.com>
2021-04-26 04:19:42 -0700

    Initial commit

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Body() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>body</p>
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <Header />
+    </div>
+  );
+}
